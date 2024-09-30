ddff4a5bcc21a9204a4d0419208aa433b3da9eab
Denis <denisbuhanov4@gmail.com>
2024-10-01 01:05:57 +0900

    5 лаба

---
+++ Лабораторные работы/Denis-buh/№ 5/People/Patient_Hospital.cs
@@ -0,0 +1,30 @@
+
+
+
+
+// Пациент больницы
+class Patient_Hospital: Human{
+    protected string code_problem; // шифр заболевания
+    public string Code{get{return this.code_problem;}}
+    protected DateOnly date_hospital; // даты госпитализации
+    public DateOnly Date_hospital{get{return this.date_hospital;}}
+
+    public Patient_Hospital(DateOnly date_birth, string gender, 
+        string surname, string name, string code_problem, 
+        DateOnly date_hospital): base(date_birth, gender, surname, name){
+        this.code_problem = code_problem; 
+        this.date_hospital = date_hospital; 
+    }
+
+    public Patient_Hospital(Human obj, string code_problem, 
+        DateOnly date_hospital): base(obj){
+        this.code_problem = code_problem; 
+        this.date_hospital = date_hospital; 
+    }
+
+    // Преобразование в строку + Вывод на экран
+    public override string Inform(){
+        return base.Inform() + $" Шифр болезни: {this.code_problem}, " +
+            $"Дата госпитализации: {this.date_hospital}"; 
+    }
+}
