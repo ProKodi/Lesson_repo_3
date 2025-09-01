6bdcc24e9a3f88d820cfe06a834c432680dc9832
Denis <denisbuhanov4@gmail.com>
2025-09-01 22:49:03 +0900

    Create very shit.py

---
+++ Функции/Denis/very shit.py
@@ -0,0 +1,27 @@
+
+
+
+
+from math import isqrt
+
+
+# Функция проверки простоты числа
+def is_prime(n: int) -> bool:
+    return n > 1 and all(n % d != 0 for d in range(2, isqrt(n) + 1))
+
+
+def main():
+    flag = False;
+
+    for i in range(int(input("Введите количество элементов: "))):
+        res = is_prime(int(input("Введите элемент: ")))
+        if(not flag): flag = res
+
+    if(flag):
+        print("Содержит простое число")
+    else:
+        print("Не содержит простых чисел")
+
+
+if __name__ == "__main__":
+    main()
