6d58a3946da21c17b1c8a745f84c66fbb9ab59d1
Denis <denisbuhanov4@gmail.com>
2025-09-08 10:10:15 +0900

    Update very shit.py

--- Функции/Denis/very shit.py
+++ Функции/Denis/very shit.py
@@ -9,12 +9,15 @@ from math import isqrt
 def is_prime(n: int) -> bool:
     return n > 1 and all(n % d != 0 for d in range(2, isqrt(n) + 1))
 
+convert = lambda number_str: int(number_str)
+
+
 
 def main():
     flag = False;
 
     for i in range(int(input("Введите количество элементов: "))):
-        res = is_prime(int(input("Введите элемент: ")))
+        res = is_prime(convert(input("Введите элемент: ")))
         if(not flag): flag = res
 
     if(flag):
