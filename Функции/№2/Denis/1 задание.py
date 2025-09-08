58903dc76d67135361321a9d9a66ae5a8869b557
Denis <denisbuhanov4@gmail.com>
2025-09-08 10:30:37 +0900

    2 лаба

---
+++ Функции/№2/Denis/1 задание.py
@@ -0,0 +1,16 @@
+
+
+
+
+def say_hello(name):
+    """Выводит приветствие с именем."""
+    print(f"Привет, {name}!")
+
+def choose_profession(profession):
+    """Выбирает профессию: Разработчик."""
+    print(f"Вы выбрали профессию: {profession}.")
+
+
+
+say_hello("Игорь");
+choose_profession("Разработчик")
---
+++ Функции/№2/Denis/3 задание.py
@@ -0,0 +1,16 @@
+
+
+
+
+def print_histogram(data):
+    """
+    Выводит гистограмму списка целых чисел.
+
+    Args:
+      data: Список целых чисел.
+    """
+    for i in data:
+        print("*" * i)
+  
+  
+print_histogram([4, 9, 7])
