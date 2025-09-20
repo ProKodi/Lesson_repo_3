fe0970cddebf10d324e314d1dbd129342e176a00
Denis <denisbuhanov4@gmail.com>
2025-09-20 11:05:28 +0900

    Мой вариант (4)
    Полностью решеное задание

---
+++ №2/Denis/Part_3_2_4_var.m
@@ -0,0 +1,7 @@
+%частотные характеристики  
+num=[2]; den=[1 0];  
+[re,im]=nyquist(num,den);  
+subplot(221); plot(re); %вещественная  
+subplot(222); plot(im); %мнимая  
+subplot(223); nyquist(num,den); %годограф  
+subplot(224); bode(num,den); %логарифмические
