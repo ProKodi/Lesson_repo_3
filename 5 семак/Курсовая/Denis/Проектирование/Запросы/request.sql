596edaa7563ae8de184506f3fba067096640b893
Denis <denisbuhanov4@gmail.com>
2025-09-30 15:27:21 +0900

    Update request.sql

--- 5 семак/Курсовая/Denis/Проектирование/Запросы/request.sql
+++ 5 семак/Курсовая/Denis/Проектирование/Запросы/request.sql
@@ -1,5 +1,14 @@
 
-/* 1 запрос*/
+/* 1 запрос */
 
 SELECT * FROM resrarunt.restatunts
   INNER JOIN resrarunt.timetable ON resrarunt.restatunts.id_timetable = resrarunt.timetable.id;
+
+
+/* 2 запрос */
+
+SELECT * FROM resrarunt.dish
+  WHERE resrarunt.dish.id_restorant = 3
+  
+;
+   
