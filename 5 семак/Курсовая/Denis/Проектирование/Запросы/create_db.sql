90bba0ed19076891639363ef72e51394ad909e2e
Denis <denisbuhanov4@gmail.com>
2025-10-07 14:20:23 +0900

    some change

--- 5 семак/Курсовая/Denis/Проектирование/Запросы/create_db.sql
+++ 5 семак/Курсовая/Denis/Проектирование/Запросы/create_db.sql
@@ -25,23 +25,76 @@ COLLATE utf8mb4_0900_ai_ci;
 Создание таблицы Должности - positions
 
 Атрибуты:
-  1) `ID должности` - id
-  2) `Название должности` - title
-  3) `Образование для должности` - education
-  4) `Название отдела` - department_name
-  5) `Описание должности` - description
-  6) `Должность являеется опасной`- is_dangerous
+  1) 'Код должности' - id
+  2) 'Название должности' - title
+  3) 'Образование для должности' - education
+  4) 'Название отдела' - department_name
+  5) 'Описание должности' - description
+  6) 'Должность являеется опасной'- is_dangerous
 */
-
-CREATE TABLE student.specialties (
-  id bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Код специальности',
-  name varchar(50) DEFAULT NULL COMMENT 'Наименование специальности',
-  `describe` text DEFAULT NULL COMMENT 'Описание специальности',
+CREATE TABLE restaurants.positions (
+  id bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Код должности',
+  title varchar(255) NOT NULL COMMENT 'Название должности',
+  education varchar(255) DEFAULT NULL COMMENT 'Образование для должности',
+  /*department_name varchar(255) NOT NULL COMMENT 'Название отдела',*/
+  description text DEFAULT NULL COMMENT 'Описание должности',
+  is_dangerous tinyint(1) DEFAULT 0 COMMENT 'Должность являеется опасной',
   PRIMARY KEY (id)
 )
 ENGINE = INNODB,
 CHARACTER SET utf8mb4,
 COLLATE utf8mb4_0900_ai_ci,
-COMMENT = 'Cпециальности';
+COMMENT = 'Должности';
+/* ------------ */
 
+
+
+/*
+Создание таблицы График работы - schedule
+
+Количество часов работы для каждого дня
+время начала работы для каждого дня
+
+Атрибуты:
+  1) 'Код графика работы' - id
+  2) 'Название должности' - title
+  3) 'Образование для должности' - education
+  4) 'Название отдела' - department_name
+  5) 'Описание должности' - description
+  6) 'Должность являеется опасной'- is_dangerous
+
+'Время начала работы в понедельник' - time_start_monday
+'Время конца  работы в понедельник' - time_end_monday
+
+'Время начала работы в вторник' - time_start_tuesday
+'Время конца  работы в вторник' - time_end_tuesday
+
+'Время начала работы в среда'
+'Время конца  работы в среда'
+
+'Время начала работы в четверг'
+'Время конца  работы в четверг'
+
+'Время начала работы в пятница'
+'Время конца  работы в пятница'
+
+'Время начала работы в суббота'
+'Время конца  работы в суббота'
+
+'Время начала работы в воскресенье'
+'Время конца  работы в воскресенье'
+*/
+CREATE TABLE restaurants.positions (
+  id bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Код должности',
+  title varchar(255) NOT NULL COMMENT 'Название должности',
+  education varchar(255) DEFAULT NULL COMMENT 'Образование для должности',
+  /*department_name varchar(255) NOT NULL COMMENT 'Название отдела',*/
+  description text DEFAULT NULL COMMENT 'Описание должности',
+  is_dangerous tinyint(1) DEFAULT 0 COMMENT 'Должность являеется опасной',
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_0900_ai_ci,
+COMMENT = 'Должности';
 /* ------------ */
