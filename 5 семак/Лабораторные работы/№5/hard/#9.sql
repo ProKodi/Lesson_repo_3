0a098fc9b109503b06ee417496f0fdd3d0f5596f
Denis <denisbuhanov4@gmail.com>
2025-10-28 13:55:13 +0900

    Что-то получилось

--- 5 семак/Лабораторные работы/№5/hard/#9.sql
+++ 5 семак/Лабораторные работы/№5/hard/#9.sql
@@ -17,19 +17,6 @@ SELECT id_student, id_discipline, mark FROM exams
 /* SELECT id_student, id_discipline, MAX(mark) FROM exams  */
 
 
-SELECT students.name, mark, disp.name FROM students
-  INNER JOIN (
-  SELECT id_student, id_discipline, mark FROM exams
-    WHERE mark = (SELECT max(mark) FROM exams)
-  ) AS max_mark
-  ON students.id = max_mark.id_student
-  
-  INNER JOIN (
-    SELECT id, name FROM disciplines
-  ) AS disp
-  ON max_mark.id_discipline = disp.id
-
-
 /*
 /* select * FROM students  */
 
@@ -63,7 +50,18 @@ SELECT sl_dicp.name, MAX(mark) FROM exams
 
 */
 
+SELECT sl_dicp.name, sl_stud.name, mark FROM exams
+  INNER JOIN (SELECT * FROM disciplines) AS sl_dicp
+  ON sl_dicp.id = exams.id_discipline
 
+  INNER JOIN (select * FROM students) AS sl_stud
+  ON sl_stud.id = exams.id_student
+
+  WHERE mark = (
+    SELECT MAX(mark) FROM exams AS new_exam
+      WHERE new_exam.id_discipline = exams.id_discipline
+  )
+  LIMIT 0, 1000
 
 
 ;
