60b5a9ef349e7c3477c75fab5d71bd5458e56c05
Denis <denisbuhanov4@gmail.com>
2025-10-28 12:48:26 +0900

    Update #6.sql

--- 5 семак/Лабораторные работы/№5/hard/#6.sql
+++ 5 семак/Лабораторные работы/№5/hard/#6.sql
@@ -5,10 +5,16 @@
 USE student;
 
 
-/* SELECT id_student FROM exams  */
-
+/*
+SELECT id_student FROM exams
+*/
+/*
 SELECT * FROM students 
-    WHERE NOT(id = ANY(SELECT id_student FROM exams))
-
+*/
+SELECT * FROM students 
+  LEFT JOIN (SELECT id_student FROM exams) AS id_st
+  ON id_st.id_student = students.id
+  WHERE id_st.id_student IS NULL
+  LIMIT 0, 1000
 
 ;
