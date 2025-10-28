4b6ea5148d7d1ba720f704d249418103d2aaf0f1
Denis <denisbuhanov4@gmail.com>
2025-10-28 12:29:39 +0900

    Update #3.sql

--- 5 семак/Лабораторные работы/№5/hard/#3.sql
+++ 5 семак/Лабораторные работы/№5/hard/#3.sql
@@ -4,8 +4,23 @@
 
 USE student;
 
+-- По хорошему можно было ограничится только вот этим (тут все равно есть список всех спец)
+-- SELECT name, `describe` FROM specialties
 
-SELECT name, `describe` FROM specialties
+
+/* 
+SELECT id_specialty, COUNT(*) FROM students
+  GROUP BY id_specialty
+
+*/
+
+SELECT name, `describe`, IFNULL(swc.cout_sp, 0) FROM specialties
+  LEFT JOIN(
+    SELECT id_specialty, COUNT(*) AS cout_sp FROM students
+      GROUP BY id_specialty
+    )  AS swc
+  ON specialties.id = swc.id_specialty
+
 
 
 ;
