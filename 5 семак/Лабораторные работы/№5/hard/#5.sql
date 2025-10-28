339041e7b631e99607d78d3c2eb76106151233c8
Denis <denisbuhanov4@gmail.com>
2025-10-28 12:44:18 +0900

    Update #5.sql

--- 5 семак/Лабораторные работы/№5/hard/#5.sql
+++ 5 семак/Лабораторные работы/№5/hard/#5.sql
@@ -18,12 +18,20 @@ SELECT id_discipline, AVG(mark) AS avg_mark  FROM exams
 USE student;
 
 
-SELECT name, groped_disciplines.avg_mark  FROM disciplines
-  INNER JOIN (
+
+/*
+
+SELECT id_discipline, AVG(mark) AS avg_mark  FROM exams
+  GROUP BY id_discipline
+
+*/
+
+SELECT name, groped_disciplines.avg_mark FROM disciplines
+  LEFT JOIN (
     SELECT id_discipline, AVG(mark) AS avg_mark  FROM exams
       GROUP BY id_discipline
   ) AS groped_disciplines
-  ON id = groped_disciplines.id_discipline
+  ON disciplines.id = groped_disciplines.id_discipline
   LIMIT 0, 1000
 
 ;
