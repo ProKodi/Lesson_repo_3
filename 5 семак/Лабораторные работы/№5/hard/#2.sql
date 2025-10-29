d2f9e02beb4807bd5250d7dd8bdf4168da112a10
Denis <denisbuhanov4@gmail.com>
2025-10-29 10:45:08 +0900

    Update #2.sql

--- 5 семак/Лабораторные работы/№5/hard/#2.sql
+++ 5 семак/Лабораторные работы/№5/hard/#2.sql
@@ -19,16 +19,22 @@ USE student;
 
 USE student;
 
-
+/* Проблема: Она хочет групировку по предметам (как фильтрация типо),
+  но если мы это сделаем то мы сможем заюзать только агрегатные функции (не получится сделать список студентов)
+*/
 SELECT students.name, `group`, date, mark, exam_discp.name  FROM students
   INNER JOIN(
     SELECT id_student, date, mark, disciplines.name FROM exams
     INNER JOIN disciplines
     ON exams.id_discipline = disciplines.id
 
-    WHERE mark >= 3
+    WHERE mark >= 3  -- AND disciplines.name = "Базы данных"
   ) AS exam_discp
   ON students.id = exam_discp.id_student
+  order by exam_discp.name -- maybe it? 
+  
   LIMIT 0, 1000
+
+;
 
 ;
