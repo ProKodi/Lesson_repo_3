d5c09a6551beeab661e7eebb08f9f336ce3cd6a1
Denis <denisbuhanov4@gmail.com>
2026-02-16 15:03:52 +0900

    Добавление проекта

---
+++ Лаб 3/Code/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+
+class Program{
+    /// <summary> 
+    /// Какая-то функция которую мы тестируем
+    /// Она очень сложная и для нее нужны тесты
+    /// </summary>
+    public static int TestFun(int a, int b){ return a + b; }
+
+
+
+    #if DEBUG
+    
+    public static void Main(string[] args){
+        Console.WriteLine("Работа приложения в режиме теста");
+        Console.WriteLine($"3 + 8 = {TestFun(3, 8)}");
+    }
+    #else
+
+    public static void Main(string[] args){
+        Console.WriteLine("Работа приложения в стандартной режиме");
+        Console.WriteLine($"3 + 8 = {TestFun(3, 8)}");
+    }
+    #endif
+
+}
