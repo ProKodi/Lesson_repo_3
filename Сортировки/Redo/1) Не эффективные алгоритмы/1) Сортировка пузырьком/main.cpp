2564cb21d4277d2bc862640cb835502d418b6991
Denis <denisbuhanov4@gmail.com>
2026-03-06 12:19:47 +0900

    Redo

---
+++ Сортировки/Redo/1) Не эффективные алгоритмы/1) Сортировка пузырьком/main.cpp
@@ -0,0 +1,80 @@
+
+
+
+
+#include <iostream>
+
+
+using namespace std;
+
+
+/*
+Так как для сортировки колекции необходимо 2 цикла 
+    с количеством итераций n (n - размер коллекции),
+    то сложность алгоритма = O(n^2)
+    
+Результатом алгоритма будет отсортированный массив 
+*/
+
+
+
+
+// сортировка по возрастанию
+void sort_ass(int* arr, const int len_arr){
+    for (int i = 0; i < len_arr; i += 1){
+        for (int ii = 0; ii < len_arr - 1; ii += 1){
+            // сравниваем элемент со следующим
+            if (arr[ii] > arr[ii + 1]){
+                // меняем местами, если следующий меньше
+                int temp = arr[ii]; 
+                arr[ii] = arr[ii + 1];
+                arr[ii + 1] = temp; 
+            }
+        }
+    }
+}
+
+
+// сортировка по убыванию
+void sort_des(int* arr, const int len_arr){
+    for (int i = 0; i < len_arr; i += 1){
+        for (int ii = 0; ii < len_arr - 1; ii += 1){
+            // сравниваем элемент со следующим
+            if (arr[ii] < arr[ii + 1]){
+                // меняем местами, если следующий больше
+                int temp = arr[ii]; 
+                arr[ii] = arr[ii + 1];
+                arr[ii + 1] = temp; 
+            }
+        }
+    }
+}
+
+
+int main(){
+    system("chcp 65001");
+    // Обьявляем колекцию которую нужно отсортировать
+    int arr[] = {4, 7, 2, 9, 7, 0, 1, 3, 6}; 
+
+    // Отправляем массив в функцию сортировки
+    sort_ass(arr, size(arr));
+
+    cout << "["; 
+    for (int i = 0; i < size(arr); i += 1){
+        cout << arr[i] << ", "; 
+    }
+    cout << "\b\b]\n"; 
+
+
+    // Отправляем массив в функцию сортировки
+    sort_des(arr, size(arr));
+
+    cout << "["; 
+    for (int i = 0; i < size(arr); i += 1){
+        cout << arr[i] << ", "; 
+    }
+    cout << "\b\b]\n"; 
+
+    system("pause");
+    return 0; 
+}
